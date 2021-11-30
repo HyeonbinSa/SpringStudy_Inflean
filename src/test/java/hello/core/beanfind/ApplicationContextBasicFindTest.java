931657a222eb9d3f934e38e46300f9f29d8c82f0
HyeonbinSa <shb1833@gmail.com>
2021-11-30 20:58:24 +0900

    test: Bean 조회 테스트 기능 생성
    Test 1. Bean 이름으로 조회
    Test 2. Bean Type으로 조회
    Test 3. Bean 구체 Type으로 조회
    
    Test 4. 등록되지 않은 Bean 이름으로 조회하여 실패

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,43 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = annotationConfigApplicationContext.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = annotationConfigApplicationContext.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로만 조회")
+    void findBeanByDetailType() {
+        MemberService memberService = annotationConfigApplicationContext.getBean(MemberServiceImpl.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 불가")
+    void findBeanByNoName() {
+//        MemberService memberService = annotationConfigApplicationContext.getBean("zxcvzxcv", MemberServiceImpl.class);
+        org.junit.jupiter.api.Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> annotationConfigApplicationContext.getBean("zxcvzxcv", MemberServiceImpl.class));
+    }
+}
