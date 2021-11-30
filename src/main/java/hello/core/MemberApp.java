a639e0b5af3084b3cc3a8956e720b2056b710a87
HyeonbinSa <shb1833@gmail.com>
2021-11-30 19:38:34 +0900

    feat: 기존 프로젝트 Springd으로 전환
    AppConfig Class에 @Configuration 어노테이션 설정을 통해 Spring으로 전환
    - 사용하는 각 메서드는 @Bean으로 등록
    - 사용 시에는 ApplicationContext를 이용하여 사용

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,22 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        /*
+        * Spring으로 변경
+        * ApplicationContext가 Spring Container
+        *  */
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); // 1. Bean으로 등록된 Method 이름, 2. 반환 타입 Class
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
