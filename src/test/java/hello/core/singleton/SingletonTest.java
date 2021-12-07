39ab8a4e16b45f071c46acd505e78c0af62648f4
HyeonbinSa <shb1833@gmail.com>
2021-12-07 21:16:38 +0900

    test: 스프링에서의 싱글톤 적용 확인 Test

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -2,21 +2,24 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
-    void pureContainer(){
+    void pureContainer() {
         AppConfig appConfig = new AppConfig();
-        MemberService  memberService1 = appConfig.memberService();
-        MemberService  memberService2 = appConfig.memberService();
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
 
-        System.out.println("memberService1 = "  + memberService1);
-        System.out.println("memberService2 = "  + memberService2);
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
 
         // 요청의 개수만큼 객체가 생성되어 참조값이 다른 것을 확인할 수 있음.
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
@@ -24,12 +27,26 @@ public class SingletonTest {
 
     @Test
     @DisplayName("싱글톤 패턴을 적용한 객체 사용")
-    void singletonServiceTest(){
+    void singletonServiceTest() {
         SingletonService singletonService1 = SingletonService.getInstance();
         SingletonService singletonService2 = SingletonService.getInstance();
 
         // same ==
         // equal = equals
         Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링과 싱글톤")
+    void springSingetonTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService 1 = " + memberService1);
+        System.out.println("memberService 2 = " + memberService2);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 }
