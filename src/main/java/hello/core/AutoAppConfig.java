8744b51eb4beb64b6fe3b35b3001cb0cbbabfe71
HyeonbinSa <shb1833@gmail.com>
2021-12-27 22:06:58 +0900

    feat: @ComponentScan 애노테이션을 이용한 Spring Bean 등록

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        // Test 를 위해 @Configuration 도 Bean 으로 등록하기때문에 예외 처리 해준다.
+)
+public class AutoAppConfig {
+
+}
