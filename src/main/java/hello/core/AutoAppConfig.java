9bec6fdc275d66f3c75d9c9d678a374a77cf52c4
HyeonbinSa <shb1833@gmail.com>
2021-12-27 23:36:25 +0900

    feat: @ComponentScan - basePackages 기능 추가

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
         // Test 를 위해 @Configuration 도 Bean 으로 등록하기때문에 예외 처리 해준다.
 )
